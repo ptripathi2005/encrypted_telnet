68c19f324f96722aebbb47637c1d663e2a152fda
Prashant Tripathi <ptripathi2005@gmail.com>
2014-05-09 00:31:00 -0500

    Identation on encrypt.c

--- encrypt.c
+++ encrypt.c
@@ -16,16 +16,18 @@ int encrypt(
     char* key,
     int key_len 
 ){
-  MCRYPT td = mcrypt_module_open(MCRYPT_ENCRYPTION_SCHEME, NULL, "cbc", NULL);
-  int blocksize = mcrypt_enc_get_block_size(td);
-  if( buffer_len % blocksize != 0 ){return 1;}
+	MCRYPT td = mcrypt_module_open(MCRYPT_ENCRYPTION_SCHEME, NULL, "cbc", NULL);
+	int blocksize = mcrypt_enc_get_block_size(td);
+	
+	if( buffer_len % blocksize != 0 ){
+		return -1;
+	}
+	mcrypt_generic_init(td, key, key_len, IV);
+	mcrypt_generic(td, buffer, buffer_len);
+	mcrypt_generic_deinit (td);
+	mcrypt_module_close(td);
 
-  mcrypt_generic_init(td, key, key_len, IV);
-  mcrypt_generic(td, buffer, buffer_len);
-  mcrypt_generic_deinit (td);
-  mcrypt_module_close(td);
-  
-  return 0;
+	return 0;
 }
 
 int decrypt(
@@ -35,47 +37,49 @@ int decrypt(
     char* key,
     int key_len 
 ){
-  MCRYPT td = mcrypt_module_open(MCRYPT_ENCRYPTION_SCHEME, NULL, "cbc", NULL);
-  int blocksize = mcrypt_enc_get_block_size(td);
-  if( buffer_len % blocksize != 0 ){return 1;}
-  
-  mcrypt_generic_init(td, key, key_len, IV);
-  mdecrypt_generic(td, buffer, buffer_len);
-  mcrypt_generic_deinit (td);
-  mcrypt_module_close(td);
-  
-  return 0;
+	MCRYPT td = mcrypt_module_open(MCRYPT_ENCRYPTION_SCHEME, NULL, "cbc", NULL);
+	int blocksize = mcrypt_enc_get_block_size(td);
+	
+	if( buffer_len % blocksize != 0 ){
+		return -1;
+	}
+	mcrypt_generic_init(td, key, key_len, IV);
+	mdecrypt_generic(td, buffer, buffer_len);
+	mcrypt_generic_deinit (td);
+	mcrypt_module_close(td);
+
+	return 0;
 }
 
 void display(char* ciphertext, int len){
-  int i = 0;
-  for (i = 0; i < len; i++){
-    printf("%d ", ciphertext[i]);
-  }
-  printf("\n");
+	int i = 0;
+	for (i = 0; i < len; i++){
+		printf("%d ", ciphertext[i]);
+	}
+	printf("\n");
 }
 
 int sym_encrypt(char* buffer,  bool flag)
 {
-  char* IV = INIT_VECTOR;
-  char *key = E_KEY;
-  int keysize = KEY_LEN;
-  int buffer_len = BUFFER_SIZE;
+	char* IV = INIT_VECTOR;
+	char *key = E_KEY;
+	int keysize = KEY_LEN;
+	int buffer_len = BUFFER_SIZE;
 #ifdef PRINT_CIPHER
-  printf("====\n");
+  	printf("====\n");
 #endif
-if (flag == true) {
-	encrypt(buffer, buffer_len, IV, key, keysize); 
+	if (flag == true) {
+		encrypt(buffer, buffer_len, IV, key, keysize); 
 #ifdef PRINT_CIPHER
-  	printf("cipher:  "); display(buffer , buffer_len);
+  		printf("cipher:  "); display(buffer , buffer_len);
 #endif
-  }
-  else {
-  	decrypt(buffer, buffer_len, IV, key, keysize);
+  	}	
+  	else {
+  		decrypt(buffer, buffer_len, IV, key, keysize);
 #ifdef PRINT_CIPHER
-  	printf("decrypt: %s\n", buffer);
+  		printf("decrypt: %s\n", buffer);
 #endif
-  }
-  return 0;
+  	}
+  	return 0;
 }
 
