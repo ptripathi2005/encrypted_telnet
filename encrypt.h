ebb49402a2e983376a8f281f7fa22d5d1e331cc1
Prashant Tripathi <ptripathi2005@gmail.com>
2014-05-08 21:02:37 -0500

    aes name changes

---
+++ encrypt.h
@@ -0,0 +1,23 @@
+#ifndef __NETWORK_H
+#define __NETWORK_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <mcrypt.h>
+
+#include <math.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#define MCRYPT_ENCRYPTION_SCHEME MCRYPT_RIJNDAEL_128
+#define INIT_VECTOR "AAAAAAAAAAAAAAAA" 
+#define E_KEY "0123456789abcdef"
+#define KEY_LEN 16
+#define BUFFER_SIZE 16 
+
+int sym_encrypt(char* buffer,  bool flag);
+
+#endif
